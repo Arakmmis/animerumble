df82d88c14256816f2e80eb2b5b097bab697de05
Marcel Christianis <marcelc6363@gmail.com>
2018-03-30 14:53:31 +0700

    Boostrap added

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ let path = require("path");
 
 const _ = require("lodash");
 app.set("view engine", "ejs");
-
-app.use(express.static(path.join(__dirname, "/")));
+app.use(express.static("public"));
 
 //Routes
 require("./app/routes/index.js")(app);
