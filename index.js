3e47dd14153b623149130242ddaed1f4c4a79b01
Marcel Christianis <marcelc6363@gmail.com>
2018-04-01 17:08:15 +0700

    Index

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ require("./app/routes/index.js")(app);
 require("./app/sockets/index.js")(http);
 
 //Initiate
-http.listen(3001, function() {
+http.listen(3001, 'localhost', function() {
   console.log("listening on *:3001");
 });
