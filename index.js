6bdd090d1a7c12ee97d87746aa2bc5ce36c51d1d
Marcel Christianis <marcelc6363@gmail.com>
2018-04-01 18:37:57 +0700

    ssl env

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ let app = require("express")();
 let http = require("http").Server(app);
 let path = require("path");
 let port = process.env.PORT || 5000;
+let env = process.env.NODE_ENV || "development";
+
+let forceSsl = function(req, res, next) {
+  if (req.headers["x-forwarded-proto"] !== "https") {
+    return res.redirect(["https://", req.get("Host"), req.url].join(""));
+  }
+  return next();
+};
 
 const _ = require("lodash");
+if (env === "production") {
+  app.use(forceSsl);
+}
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
