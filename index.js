afd067e44d709d20e5d56271a20db32da1353b5f
Marcel Christianis <marcelc6363@gmail.com>
2018-04-01 17:17:20 +0700

    Change Port

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ require("./app/routes/index.js")(app);
 require("./app/sockets/index.js")(http);
 
 //Initiate
-http.listen(3001, 'localhost', function() {
-  console.log("listening on *:3001");
+http.listen(3000, 'localhost', function() {
+  console.log("listening on *:3000");
 });
