6eaf46b7e6e080f1559e6e6c67b1ac43f84de426
Marcel Christianis <marcelc6363@gmail.com>
2018-11-10 10:23:00 +0700

    edit security

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 let credentials = {}
 
-credentials.mongodb = "mongodb://admin:marcel@35.196.30.65:27017/animerumble"
+credentials.mongodb = ""
 
 credentials.imgur = {
-    clientId: "Client-ID 1220d5eb79e65d6",
-    brearer: "Bearer b096101df77531583ad23939da5569c873032e67"
+    clientId: "",
+    brearer: ""
 }
 
 module.exports = credentials
